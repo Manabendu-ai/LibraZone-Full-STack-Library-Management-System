c4e27eb1feb846b5495867adec91228d4a55b32d
Manabendu-ai <rikugithub@gmail.com>
2025-12-27 09:07:01 +0530

    update the deafult landing page

--- src/main/java/riku/spring/controllers/AdminController.java
+++ src/main/java/riku/spring/controllers/AdminController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -78,6 +79,11 @@ public class AdminController {
             }
             return "AdminNotFound";
 
+    }
+
+    @GetMapping("/")
+    public String defaultPage(){
+        return "redirect:/login";
     }
 
 }
